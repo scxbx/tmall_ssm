4474a514acdedbbd898639dba465925f25b9da25
scxbx <761189437@qq.com>
2019-12-22 17:05:47 +0800

    add

---
+++ src/main/java/com/how2java/tmall/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.how2java.tmall.controller;
+
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.Page;
+import com.how2java.tmall.util.UploadedImageFile;
+import com.how2java.tmall.util.ImageUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+@Controller
+@RequestMapping("")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @RequestMapping("admin_category_list")
+    public String list(Model model,Page page){
+        List<Category> cs= categoryService.list(page);
+        int total = categoryService.total();
+        page.setTotal(total);
+        model.addAttribute("cs", cs);
+        model.addAttribute("page", page);
+        return "admin/listCategory";
+    }
+
+    @RequestMapping("admin_category_add")
+    public String add(Category c, HttpSession session, UploadedImageFile uploadedImageFile) throws IOException {
+        categoryService.add(c);
+        File  imageFolder= new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,c.getId()+".jpg");
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        uploadedImageFile.getImage().transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
+        return "redirect:/admin_category_list";
+    }
+}
